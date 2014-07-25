ca89621dedaed5cf815b4bbf5b029b5f35db4568
Amit Patel <CHETU\amitp@chetuiwk430.chetu.com>
2014-07-25 17:26:59 +0530

    completed third cahpter rails tutorial

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -9,7 +11,7 @@ describe "StaticPages" do
     end
 		it "should have the right title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).to have_title("#{base_title} | Home")
 		end
 
   end
@@ -22,7 +24,7 @@ describe "StaticPages" do
     end
 		it "should have the right title" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 
   end
@@ -35,7 +37,20 @@ describe "StaticPages" do
     end
 		it "should have the right title" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+			expect(page).to have_title("#{base_title} | About Us")
+		end
+
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact Us")
 		end
 
   end
