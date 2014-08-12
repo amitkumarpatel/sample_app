449337fb246b4fd5d59adf81bc4be7ccdff9f323
Amit Patel <CHETU\amitp@chetuiwk430.chetu.com>
2014-08-12 21:55:48 +0530

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ before_action :admin_user,     only: :destroy
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -54,13 +55,6 @@ before_action :admin_user,     only: :destroy
                                      :password_confirmation)
       end
      # Before filters
-
-      def signed_in_user
-        unless signed_in?
-          store_location
-          redirect_to signin_url, notice: "Please sign in."
-        end
-      end    
     
      def correct_user
       @user = User.find(params[:id])
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -63,10 +63,19 @@ describe "UserPages" do
 
 	describe "profile page" do
 		let(:user) { FactoryGirl.create(:user) }# Replace with code to make a user variable
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
 		before { visit user_path(user) }
 
 		it { should have_content(user.name) }
 		it { should have_title(user.name) }
+    
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end		
 	end
 
   describe "signup" do
@@ -94,7 +103,7 @@ describe "UserPages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
